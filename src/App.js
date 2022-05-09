b466d45980380e4511c6e0100bc2d283a552ba81
Jhonatan Cueva <jc@Jhonatans-MacBook-Pro.local>
2022-05-09 10:26:05 -0500

    2-status

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import { useState } from "react";
 import "./App.css";
 import Mensaje from "./components/mensaje";
 
 function App() {
+  const [name, setName] = useState("");
+
   return (
     <div className="App">
-      <Mensaje props_firstName="liam " props_lastName="cave" props_age="26" />
-      <Mensaje props_firstName="liam " props_lastName="cave" props_age="26" />
-      <Mensaje props_firstName="liam " props_lastName="cave" props_age="26" />
-      <Mensaje props_firstName="liam " props_lastName="cave" props_age="26" />
-      <Mensaje props_firstName="liam " props_lastName="cave" props_age="26" />
+      <input
+        type="text"
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+      <Mensaje props_firstName={name} props_lastName="cave" props_age="26" />
     </div>
   );
 }
